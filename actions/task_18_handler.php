038d792fa8b95353df2feb486ac308cbe0144423
--replace-all <=>
2022-06-05 15:19:03 +0800

    complete task 20

--- actions/task_18_handler.php
+++ actions/task_18_handler.php
@@ -1,26 +1,36 @@
 <?php
 session_start();
 require_once __DIR__ . '/../db.php';
-
-$file_name = uniqid() . '.' .pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);//получаем расширение файла
 $file_path = __DIR__ . '/../img/users_images/';
-if (is_uploaded_file($_FILES['image']['tmp_name'])){
-    $destination = $file_path . $file_name;
-    var_dump($destination);
+function upload_file($file_name, $tmp_name)
+{
+    if (is_uploaded_file($tmp_name)){
+        /**@var $file_path*/
+        global $file_path;
+        $destination = $file_path . $file_name;
+        var_dump($destination);
 
-    move_uploaded_file($_FILES['image']['tmp_name'], $destination);
-    /**@var $pdo*/
-    $stmt = $pdo->prepare('INSERT INTO task18 (image) VALUE (:image)');
-    $stmt->execute([':image' => $file_name]);
-    $_SESSION['success'] = 'Картинка успешно загружена';
-    header("Location: /../task_18.php");
-    die();
-} else {
-    $_SESSION['error'] = "ошибка при добавлении изображения";
-    header("Location: /../task_18.php");
-    die();
+        move_uploaded_file($tmp_name, $destination);
+        /**@var $pdo*/
+        global $pdo;
+        $stmt = $pdo->prepare('INSERT INTO task18 (image) VALUE (:image)');
+        $stmt->execute([':image' => $file_name]);
+        $_SESSION['success'] = 'Картинка успешно загружена';
+
+    } else {
+        $_SESSION['error'] = "ошибка при добавлении изображения";
+
+    }
+
 }
 
+for ($i = 0; $i <= count($_FILES['image']['name']); $i++ ) {
+    $file_name = uniqid() . '.' .pathinfo($_FILES['image']['name'][$i], PATHINFO_EXTENSION);//получаем расширение файла
+
+    upload_file($file_name, $_FILES['image']['tmp_name'][$i]);
+}
+header("Location: /../task_20.php");
+die();
 
 
 
